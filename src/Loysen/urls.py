2e5c5b978a077853701d87f6c14748d938ea2202
Kiel Loysen <kielpedia@gmail.com>
2012-12-12 15:59:56 -0500

    adding default banner image which looks like crap

--- src/Loysen/urls.py
+++ src/Loysen/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic.list import ListView
-from django.views.generic.simple import direct_to_template
+from django.views.generic.base import TemplateView
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -9,7 +9,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     (r'^$', 'views.index'),
-    (r'^about$', direct_to_template, {'template': 'about.html'}),
+    (r'^about$', TemplateView.as_view(template_name='about.html')),
     (r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
     # url(r'^$', 'Loysen.views.home', name='home'),
